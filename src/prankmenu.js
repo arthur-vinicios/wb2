8c0dc17ef5638ea7028f85e61435f70ba717323d
arthur-vinicios <48295687+arthur-vinicios@users.noreply.github.com>
2021-05-04 17:20:42 -0300

    Delete src directory

--- src/prankmenu.js
+++
@@ -1,9 +0,0 @@
-const prankmenu = (prefix) => {
-	return `
-Grupos Oficiais 
-Grupo 1: https://chat.whatsapp.com/Faya6RMm7tS2nMfhtWsEDR
-Grupo 2: https://chat.whatsapp.com/Eoa9BcYIoKN9d483ZpOVZn
-`
-}
-
-exports.prankmenu = prankmenu
